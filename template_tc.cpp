fae85fd9cb7c77de84e9afbe22b34ea9cb518695
yhy_jp <yhyhy.jp@gmail.com>
2013-09-12 03:40:21 +0900

    topcoder

---
+++ template_tc.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <set>
+#include <stack>
+#include <bitset>
+#include <utility>
+#include <functional>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+
+class $CLASSNAME$
+{
+public:
+	$RC$ $METHODNAME$($METHODPARMS$)
+	{
+	
+	}
+
+
+	$TESTCODE$
+};
+
+// BEGIN CUT HERE
+int main()
+{
+	$CLASSNAME$ ___test;
+	___test.run_test(-1);
+
+	return 0;
+}
+// END CUT HERE
